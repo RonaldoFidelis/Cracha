dacb49b5c1f36e877a92fb0471b9159e41635b0a
Ronaldo <ronaldofidelis.ti@gmail.com>
2023-10-08 19:24:49 -0300

    Feat: ChangedProject

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily:{
-        'poppins': ["'Poppins', sans-serif"]
-      },
-      width: {
-        '90px':'92px',
-        'larguraMd':'110px',
-        '50px':'50px'
-      },
-      height: {
-        '90px':'92px',
-        'alturaMd':'110px',
-        '50px':'50px'
-      },
       colors: {
-        'black':'#000000',
-        'main':'#000C24',
-        'second':'#011140',
-        'ft':'#38B6FF',
-        'border': '#016394',
-        'git':'#9C34C2',
-        'linkedin':'#0078D4'
+        "font":"#FFFFFF"
       }
     },
   },
